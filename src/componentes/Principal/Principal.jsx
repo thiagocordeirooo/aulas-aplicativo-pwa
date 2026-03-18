dd3f99b98aca1a79dce9647d701adb176d8eb661
Thiago Cordeiro <thiagocordeirooo@gmail.com>
2026-03-17 21:14:05 -0300

    criação do BotaoCustomizado

--- src/componentes/Principal/Principal.jsx
+++ src/componentes/Principal/Principal.jsx
@@ -1,7 +1,14 @@
+import BotaoCustomizado from "../BotaoCustomizado/BotaoCustomizado";
 import "./Principal.css";
 
 function Principal() {
-  return <main className="principal__root">Principal</main>;
+  return (
+    <main className="principal__root">
+      <BotaoCustomizado tipo="primario">Salvar</BotaoCustomizado>
+
+      <BotaoCustomizado tipo="secundario">Cancelar</BotaoCustomizado>
+    </main>
+  );
 }
 
 export default Principal;
