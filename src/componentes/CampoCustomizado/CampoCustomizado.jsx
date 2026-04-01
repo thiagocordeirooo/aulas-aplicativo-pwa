a2601a7d41ed1005ab6d42a8d1e03fb8898a9cd3
Thiago Cordeiro <thiagocordeirooo@gmail.com>
2026-03-31 21:04:05 -0300

    continuação ListaTarefas

--- src/componentes/CampoCustomizado/CampoCustomizado.jsx
+++ src/componentes/CampoCustomizado/CampoCustomizado.jsx
@@ -3,7 +3,7 @@ import "./CampoCustomizado.css";
 function CampoCustomizado({ label, ...props }) {
   return (
     <div className="campo-customizado__root">
-      <label>{label}</label>
+      <span>{label}</span>
       <input className="campo-customizado__input" {...props} />
     </div>
   );
--- src/paginas/ListaTarefas/ListaTarefas.jsx
+++ src/paginas/ListaTarefas/ListaTarefas.jsx
@@ -1,9 +1,22 @@
 import { useState } from "react";
-import Principal from "../../componentes/Principal/Principal";
+import BotaoCustomizado from "../../componentes/BotaoCustomizado/BotaoCustomizado";
 import CampoCustomizado from "../../componentes/CampoCustomizado/CampoCustomizado";
+import Principal from "../../componentes/Principal/Principal";
 
 function ListaTarefas({}) {
   const [novaTarefa, setNovaTarefa] = useState("");
+  const [tarefas, setTarefas] = useState([]);
+
+  const adicionarTarefa = () => {
+    if (novaTarefa.trim() === "") {
+      alert("Digite uma tarefa para adicionar!");
+      return;
+    }
+
+    console.log(novaTarefa);
+
+    setTarefas([novaTarefa, ...tarefas]);
+  };
 
   return (
     <Principal>
@@ -11,11 +24,19 @@ function ListaTarefas({}) {
 
       <CampoCustomizado
         label="Nova Tarefa"
+        // proprieade não mapeadas...
+        id="nova-tarefa"
         type="text"
         placeholder="Precione Enter para adicionar"
         value={novaTarefa}
         onChange={(event) => setNovaTarefa(event.target.value)}
       />
+
+      <BotaoCustomizado tipo="secundario" aoClicar={adicionarTarefa}>
+        +
+      </BotaoCustomizado>
+
+      {JSON.stringify(tarefas, null, 2)}
     </Principal>
   );
 }
